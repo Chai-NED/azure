3cbed522a2c919b759e8d625fbf2ef35725eb69f
Patrick El-Azem <paelaz@microsoft.com>
2018-02-08 18:18:38 -0500

    Azure PS1 minor cleanup

--- ps1-azure/ResourceGroup-DeleteResources.ps1
+++ ps1-azure/ResourceGroup-DeleteResources.ps1
@@ -8,9 +8,9 @@
 # Arguments with defaults
 param
 (
-    [string]$SubscriptionId = 'e61e4c75-268b-4c94-ad48-237aa3231481',
-    [string]$ResourceGroupName = 'pz17-vm',
-    [string]$NamePrefixToDelete = 'pz17vm5'
+    [string]$SubscriptionId = '',
+    [string]$ResourceGroupName = '',
+    [string]$NamePrefixToDelete = ''
 )
 
 # Delete VMs first to remove any leases
